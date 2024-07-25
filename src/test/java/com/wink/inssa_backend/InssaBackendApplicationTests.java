63f6ecaaaea6469f76d881cb6be5f0e9d81d5989
daun-up <awhekdns@kookmin.ac.kr>
2024-07-25 14:23:15 +0900

    Initial Settings

---
+++ src/test/java/com/wink/inssa_backend/InssaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wink.inssa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InssaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
