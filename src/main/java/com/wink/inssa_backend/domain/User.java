65b0084095d0c080079c86f80e7e061abdf3dfcd
daun-up <awhekdns@kookmin.ac.kr>
2024-07-25 17:34:40 +0900

    feat: 코드 구현

---
+++ src/main/java/com/wink/inssa_backend/domain/User.java
@@ -0,0 +1,75 @@
+package com.wink.inssa_backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails { // UserDetails를 상속받아 인증 객체로 사용
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    @Override // 권한 반환
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired(){
+        // 만료되었는지 확인하는 로직
+        return true; // true -> 만료되지 않음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked(){
+        return true; // true -> 잠금되지 않음
+    }
+
+    // 패스워드 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true; // true -> 만료되지 않음
+    }
+
+    // 계정 사용 가능 여부 변환
+    @Override
+    public boolean isEnabled(){
+        return true; // true -> 사용 가능
+    }
+}
---
+++ src/main/java/com/wink/inssa_backend/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package com.wink.inssa_backend.service;
+
+import lombok.RequiredArgsConstructor;
+import com.wink.inssa_backend.domain.User;
+import com.wink.inssa_backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 사용자 이름(email)으로 사용자 정보를 가져오는 메소드
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
